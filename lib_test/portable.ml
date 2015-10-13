08a72293743b5404a99f97331b54f7b9ba72e492
Matt Gray <matt.gray@7digital.com>
2015-10-13 11:41:02 +0100

    Test more of CLOCK

--- lib_test/portable.ml
+++ lib_test/portable.ml
@@ -1,6 +1,20 @@
 module C = Clock
 
+let print_time () =
+  let d, ps = C.now_d_ps () in
+  Printf.printf "The time is %d days and %Ld picoseconds since the epoch.\n" d ps
+
+let print_offset () =
+  let offset = C.current_tz_offset_s () in
+  Printf.printf "The offset from UTC is %d minutes.\n" offset
+
+let print_period () = match C.period_d_ps () with
+  | Some (d, ps) -> Printf.printf "The clock period is: %Ld picoseconds\n" ps
+  | None -> Printf.printf "Clock period unavailable\n"
+
 let _ =
-  match C.period_d_ps () with
-    | Some (d, ps) -> Printf.printf "The clock period is: %Ld picoseconds\n" ps
-    | None -> Printf.printf "Clock period unavailable\n"
+  print_time ();
+  print_time ();
+  print_time ();
+  print_offset ();
+  print_period ();
