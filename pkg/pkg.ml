ee5014dfa0379448b4ffe863e2b18ed39effedb6
Martin Lucina <martin.lucina@docker.com>
2016-11-16 16:33:48 +0100

    Move META files under pkg/

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -6,8 +6,8 @@ open Topkg
 let () =
   let install = false in
   let metas = [
-    Pkg.meta_file ~install "unix/META";
-    Pkg.meta_file ~install "freestanding/META" ]
+    Pkg.meta_file ~install "pkg/META.unix";
+    Pkg.meta_file ~install "pkg/META.freestanding" ]
   in
   let lint_deps_excluding = Some ["mirage-types-lwt"] in
   let opams = [
@@ -17,12 +17,12 @@ let () =
   Pkg.describe ~metas ~opams "mirage-clock-unix" @@ fun c ->
   match Conf.pkg_name c with
   | "mirage-clock-unix" ->
-      Ok [  Pkg.lib "unix/META";
+      Ok [  Pkg.lib "pkg/META.unix" ~dst:"META";
             Pkg.mllib "unix/mirage-clock-unix.mllib";
             Pkg.clib "unix/libmirage-clock-unix_stubs.clib";
             Pkg.test "lib_test/portable"]
   | "mirage-clock-freestanding" ->
-      Ok [  Pkg.lib "freestanding/META";
+      Ok [  Pkg.lib "pkg/META.freestanding" ~dst:"META";
             Pkg.mllib "freestanding/mirage-clock-freestanding.mllib" ]
   | other ->
       R.error_msgf "unknown package name: %s" other
