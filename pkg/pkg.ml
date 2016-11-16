2dc38faea621a094a8e13d25f47d3707dabd97b0
Martin Lucina <martin.lucina@docker.com>
2016-11-16 16:20:04 +0100

    xen: Rename to mirage-clock-freestanding
    The -xen implementation is also used by Solo5, rename to -freestanding.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -7,12 +7,12 @@ let () =
   let install = false in
   let metas = [
     Pkg.meta_file ~install "unix/META";
-    Pkg.meta_file ~install "xen/META" ]
+    Pkg.meta_file ~install "freestanding/META" ]
   in
   let lint_deps_excluding = Some ["mirage-types-lwt"] in
   let opams = [
     Pkg.opam_file ~install ~lint_deps_excluding "mirage-clock-unix.opam";
-    Pkg.opam_file ~install ~lint_deps_excluding "mirage-clock-xen.opam" ]
+    Pkg.opam_file ~install ~lint_deps_excluding "mirage-clock-freestanding.opam" ]
   in
   Pkg.describe ~metas ~opams "mirage-clock-unix" @@ fun c ->
   match Conf.pkg_name c with
@@ -21,8 +21,8 @@ let () =
             Pkg.mllib "unix/mirage-clock-unix.mllib";
             Pkg.clib "unix/libmirage-clock-unix_stubs.clib";
             Pkg.test "lib_test/portable"]
-  | "mirage-clock-xen" ->
-      Ok [  Pkg.lib "xen/META";
-            Pkg.mllib "xen/mirage-clock-xen.mllib" ]
+  | "mirage-clock-freestanding" ->
+      Ok [  Pkg.lib "freestanding/META";
+            Pkg.mllib "freestanding/mirage-clock-freestanding.mllib" ]
   | other ->
       R.error_msgf "unknown package name: %s" other
